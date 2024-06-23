711ff77dcce23e0fa415f89003bc74038c9167c6
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-06-23 20:36:17 +0530

    wip

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u8) -> u8 {
+    match count {
+        35 => 70,
+        40 => 80,
+        41 => 41,
+        65 => 65,
+        _ => 0,
+    }
+}
 
 // Don't modify this function!
 #[test]
