b24bac99c946b1e4153b46c1a35097461cd19c66
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-06-24 12:07:06 +0530

    wip

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,14 @@
 use std::{cmp::Ordering, io};
 
 use rand::Rng;
-
 fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..100);
     loop {
         println!("Guess a num and input");
         let mut guess: String = String::new();
         let res: Result<usize, io::Error> = io::stdin().read_line(&mut guess);
+        let mut v = vec![12];
+        v.push(11);
 
         match res {
             Ok(size) => {
@@ -44,4 +45,22 @@ fn main() {
             }
         }
     }
+}
+
+fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    for element in v.iter_mut() {
+        // TODO: Fill this up so that each element in the Vec `v` is
+        // multiplied by 2.
+    }
+
+    // At this point, `v` should be equal to [4, 8, 12, 16, 20].
+    v
+}
+
+fn vec_map(v: &Vec<i32>) -> Vec<i32> {
+    v.iter().map(|element| {
+        // TODO: Do the same thing as above - but instead of mutating the
+        // Vec, you can just return the new number!
+        *element
+    }).collect()
 }
