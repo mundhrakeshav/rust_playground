50f8b2f8d8fedba4d792bd4054d1dbe6d86c35b7
Keshav Mundhra <mundhrakeshav11@gmail.com>
2024-07-20 00:32:23 +0530

    play

---
+++ playground/src/main.rs
@@ -0,0 +1,38 @@
+use std::sync::Mutex;
+use std::thread;
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+fn main() {
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // println!("Result: {}", *counter.lock().unwrap());
+}
