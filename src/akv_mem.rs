d004de55ce7e34664e3a0ff59b0817fd25601af9
Keshav Mundhra <mundhrakeshav11@gmail.com>
2025-01-20 17:53:09 +0530

    Refactor main function in akv_mem.rs to comment out action handling logic for future implementation

--- src/akv_mem.rs
+++ src/akv_mem.rs
@@ -20,23 +20,22 @@ fn main() {
     store.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", value),
-        },
+        //     "get" => match store.get(key).unwrap() {
+        //         None => eprintln!("{:?} not found", key),
+        //         Some(value) => println!("{:?}", value),
+        //     },
 
-        "delete" => store.delete(key).unwrap(),
+        //     "delete" => store.delete(key).unwrap(),
 
-        "insert" => {
-            let value = maybe_value.expect(&USAGE).as_ref();
-            store.insert(key, value).unwrap()
-        }
+        //     "insert" => {
+        //         let value = maybe_value.expect(&USAGE).as_ref();
+        //         store.insert(key, value).unwrap()
+        //     }
 
-        "update" => {
-            let value = maybe_value.expect(&USAGE).as_ref();
-            store.update(key, value).unwrap()
-        }
-
+        //     "update" => {
+        //         let value = maybe_value.expect(&USAGE).as_ref();
+        //         store.update(key, value).unwrap()
+        //     }
         _ => eprintln!("{}", &USAGE),
     }
 }
