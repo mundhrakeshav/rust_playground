d9dd1a8245b53ddf7a450e0fdc5373ef5b691924
Keshav Mundhra <mundhrakeshav11@gmail.com>
2025-01-27 22:47:25 +0530

    Update akv_disk.rs and Cargo.toml for disk-based key-value store CLI

--- src/akv_disk.rs
+++ src/akv_disk.rs
@@ -1,40 +1,79 @@
 use libactionkv::ActionKV;
+use std::collections::HashMap;
+
+#[cfg(target_os = "windows")]
 const USAGE: &str = "
 Usage:
-    akv_mem FILE get KEY
-    akv_mem FILE delete KEY
-    akv_mem FILE insert KEY VALUE
-    akv_mem FILE update KEY VALUE
+    akv_disk.exe FILE get KEY
+    akv_disk.exe FILE delete KEY
+    akv_disk.exe FILE insert KEY VALUE
+    akv_disk.exe FILE update KEY VALUE
 ";
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+Usage:
+    akv_disk FILE get KEY
+    akv_disk FILE delete KEY
+    akv_disk FILE insert KEY VALUE
+    akv_disk FILE update KEY VALUE
+";
 
+type ByteStr = [u8];
+type ByteString = Vec<u8>;
+
+fn store_index_on_disk(a: &mut ActionKV, index_key: &ByteStr) {
+    a.index.remove(index_key);
+    let index_as_bytes = bincode::serialize(&a.index).unwrap();
+    a.index = std::collections::HashMap::new();
+    a.insert(index_key, &index_as_bytes).unwrap();
+}
+
+fn main() {
+    const INDEX_KEY: &ByteStr = b"+index";
+
+    let args: Vec<String> = std::env::args().collect();
     let fname = args.get(1).expect(&USAGE);
-    let action: &str = args.get(2).expect(&USAGE).as_ref();
+    let action = args.get(2).expect(&USAGE).as_ref();
     let key = args.get(3).expect(&USAGE).as_ref();
     let maybe_value = args.get(4);
 
     let path = std::path::Path::new(&fname);
-    let mut store = ActionKV::open(path).expect("unable to open file");
-    store.load().expect("unable to load data");
+    let mut a = ActionKV::open(path).expect("unable to open file");
+
+    a.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", String::from_utf8(value).expect("error ")),
-        },
+        "get" => {
+            let index_as_bytes = a.get(&INDEX_KEY)
+                .unwrap()
+                .unwrap();
 
-        "delete" => store.delete(key).unwrap(),
+            let index_decoded = bincode::deserialize(&index_as_bytes);
+
+            let index: HashMap<ByteString, u64> = index_decoded.unwrap();
+
+            match index.get(key) {
+                None => eprintln!("{:?} not found", key),
+                Some(&i) => {
+                    let kv = a.get_at(i).unwrap();
+                    println!("{:?}", kv.value)                
+                }
+            }
+        }
+
+        "delete" => a.delete(key).unwrap(),
 
         "insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
-            store.insert(key, value).unwrap()
+            a.insert(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);   
         }
 
         "update" => {
             let value = maybe_value.expect(&USAGE).as_ref();
-            store.update(key, value).unwrap()
+            a.update(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);       
         }
         _ => eprintln!("{}", &USAGE),
     }
