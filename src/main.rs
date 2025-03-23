9ed1d5a3a8244d75e3455940be96f0996de52a2d
Keshav Mundhra <mundhrakeshav11@gmail.com>
2025-03-23 13:53:28 +0530

    Add DNS lookup tool with Clap CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use clap::Parser;
+use std::{
+    net::{SocketAddr, UdpSocket},
+    time::Duration,
+};
+use trust_dns_client::{
+    op::{Message, MessageType, OpCode, Query},
+    rr::{Name, RecordType},
+    serialize::binary::{BinEncodable, BinEncoder},
+};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(short = 's', long, default_value_t = ("1.1.1.1").to_string())]
+    dns_server: String,
+    #[arg(short = 'd', long)]
+    domain_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let domain_name = Name::from_ascii(&args.domain_name).expect("Invalid domain name");
+    let dns_server: SocketAddr = format!("{}:53", args.dns_server)
+        .parse()
+        .expect("Invalid DNS server address");
+
+    let mut request_as_bytes: Vec<u8> = Vec::with_capacity(512);
+    let mut response_as_bytes = vec![0; 512];
+
+    let mut msg = Message::new();
+    let msg_id = 100;
+    msg.set_id(msg_id)
+        .set_message_type(MessageType::Query)
+        .add_query(Query::query(domain_name, RecordType::A))
+        .set_op_code(OpCode::Query)
+        .set_recursion_desired(true);
+
+    let mut encoder = BinEncoder::new(&mut request_as_bytes);
+    msg.emit(&mut encoder).expect("failed to encode");
+
+    let localhost = UdpSocket::bind("0.0.0.0:0").expect("Cannot bind to local socket");
+    localhost
+        .set_read_timeout(Some(Duration::from_secs(10)))
+        .unwrap();
+
+    localhost.set_nonblocking(false).unwrap();
+
+    localhost
+        .send_to(&request_as_bytes, dns_server)
+        .expect("Failed to send DNS request");
+
+    let (_amt, _remote) = localhost
+        .recv_from(&mut response_as_bytes)
+        .expect("Timeout reached");
+
+    let dns_message = Message::from_vec(&response_as_bytes).expect("Unable to parse response");
+
+    for answer in dns_message.answers() {
+        if let Some(ip) = answer.data().and_then(|r| r.as_a()) {
+            println!("ip: {}", ip);
+        }
+    }
 }
